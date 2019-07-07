ea42adbf4952f72c36c71a0a278289c9d0a20af2
Kaja <catcalledkaja@gmail.com>
2019-07-07 11:16:26 +0100

    User story 2 with updated takeaway class

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,20 +1,29 @@
 require 'takeaway'
 
 describe Takeaway do
-subject(:takeaway) { described_class.new(menu: menu) }
+subject(:takeaway) { described_class.new(menu: menu, order: order) }
 let(:menu) { double(:menu, print: printed_menu) }
 let(:printed_menu) { "Printed Menu" }
-
+let(:order) { double :order }
+let(:dishes) do
+  {
+    chicken: 2,
+    chips: 3,
+  }
+end
 describe "#print_menu" do
   it "Shows Menu with lists and prices" do
     expect(takeaway.print_menu).to eq(printed_menu)
   end
 end
-# 
+#
 # As a customer
 # So that I can verify that my order is correct
 # I would like to check that the total I have been given matches the sum of the various dishes in my order
-
+  it "can order some number of several dishes" do
+    expect(order).to receive(:add).twice
+    takeaway.place_order(dishes)
+  end
 
 
 end
